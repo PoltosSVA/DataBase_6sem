377f458e50471568ec8f049a5c2368f47b3adc88
Vladislav Stovba <94395154+PoltosSVA@users.noreply.github.com>
2024-02-14 18:48:06 +0300

    Added same id check

--- Task_4/task_4.sql
+++ Task_4/task_4.sql
@@ -1,8 +1,24 @@
-CREATE OR REPLACE FUNCTION generate_insert_statement(p_id NUMBER) RETURN VARCHAR2
-	IS
-        	v_insert_statement VARCHAR2(4000);
-	BEGIN
-        	v_insert_statement := 'INSERT INTO MyTable(id,val) VALUES('||p_id||','||ROUND(dbms_random.VALUE(1,10000))||');';
-        
-        	RETURN v_insert_statement;
-	END generate_insert_statement;
+CREATE OR REPLACE FUNCTION generate_insert_statement(p_id VARCHAR2) RETURN VARCHAR2
+IS
+        v_insert_statement VARCHAR2(4000):='NONE';
+        v_temp_check NUMBER;
+        v_id NUMBER;
+BEGIN
+        v_id:=TO_NUMBER(p_id);
+
+        SELECT COUNT(*)INTO v_temp_check from MyTable
+        WHERE id = v_id;
+
+        IF v_temp_check = 1 THEN
+            v_insert_statement := 'This ID is already in the table.';
+        ELSIF v_temp_check = 0 THEN
+            v_insert_statement := 'INSERT INTO MyTable(id,val) VALUES('||v_id||','||ROUND(dbms_random.VALUE(1,10000))||');';
+        ELSE
+            RAISE_APPLICATION_ERROR(-20001,'Something wrong.');
+        END IF;
+
+        RETURN v_insert_statement;
+EXCEPTION
+     WHEN VALUE_ERROR THEN
+            DBMS_OUTPUT.PUT_LINE('Argument error' || SQLERRM);
+END generate_insert_statement;
